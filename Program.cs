d40f67186342b9b6dbae2bd196a78682b765c9e8
Nick <nicholas.a.pawlawski@wmich.edu>
2018-06-11 11:09:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareConfiguation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigReader cr = ConfigReader.GetConfigReader();
+        
+            Reporter.Reporter.StartReporter(cr.FolderName);
+
+            Console.WriteLine(SoftwareConfiguration.TestValue);
+            Console.ReadLine();
+
+        }
+    }
+}
